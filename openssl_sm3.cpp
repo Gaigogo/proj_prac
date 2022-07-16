92b5b240bedba75a3a93319d3243cb4bc6eb94ba
Gaigogo <105497888+Gaigogo@users.noreply.github.com>
2022-07-16 10:31:44 +0800

    Create openssl_sm3.cpp

--- openssl_sm3.cpp
+++ openssl_sm3.cpp
@@ -4,18 +4,18 @@
 #include<time.h>
 void calcul()
 {
-    unsigned char output[EVP_MAX_MD_SIZE];   //保存输出数组
+    unsigned char output[EVP_MAX_MD_SIZE];   //淇濆瓨杈撳嚭鏁扮粍
     unsigned int len, i;
-    EVP_MD_CTX* ctx;                         //消息摘要
-    ctx = EVP_MD_CTX_new();//调用函数初始化
-    char msg1[] = "20220705";              //待计算的消息
-    EVP_MD_CTX_init(ctx);                    //初始化摘要结构体
-    EVP_DigestInit_ex(ctx, EVP_sm3(), NULL); //设置摘要算法和密码算法引擎，这里密码算法使用sm3，算法引擎使用OpenSSL默认引擎即软算法
-    EVP_DigestUpdate(ctx, msg1, strlen(msg1));//调用摘要UpDate计算msg1的摘要
-    EVP_DigestFinal_ex(ctx, output, &len);//摘要结束，输出摘要值   
-    EVP_MD_CTX_reset(ctx);                       //释放内存
+    EVP_MD_CTX* ctx;                         
+    ctx = EVP_MD_CTX_new();
+    char msg1[] = "20220705";              
+    EVP_MD_CTX_init(ctx);                    
+    EVP_DigestInit_ex(ctx, EVP_sm3(), NULL); 
+    EVP_DigestUpdate(ctx, msg1, strlen(msg1));
+    EVP_DigestFinal_ex(ctx, output, &len);  
+    EVP_MD_CTX_reset(ctx);                       
 
-    printf("%s的hash:\n", msg1);
+    printf("%s鐨刪ash:\n", msg1);
     for (i = 0; i < len; i++)
     {
         printf("0x%x ", output[i]);
