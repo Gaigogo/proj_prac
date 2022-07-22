f32e1e491a8674ea4c8759cc2f84e5674342e413
Gaigogo <105497888+Gaigogo@users.noreply.github.com>
2022-07-22 21:37:37 +0800

    Add files via upload

---
+++ sm2 pitfalls/two user.py
@@ -0,0 +1,71 @@
+import math
+import random
+import time
+import hashlib
+
+#1.Eculid求最大公因子
+def gcd(a,b):
+    r=a%b
+    while(r!=0):
+        a=b
+        b=r
+        r=a%b
+    return b
+
+#2.求模逆的扩展欧几里得
+def ModInverse(a,m):
+    if gcd(a,m)!=1:
+        return None
+    u1,u2,u3=1,0,a
+    v1,v2,v3=0,1,m
+    while v3!=0:
+        q=u3//v3
+        v1,v2,v3,u1,u2,u3=((u1-q*v1),(u2-q*v2),(u3-q*v3),v1,v2,v3)
+    return u1%m
+def get_hash(string0):
+    sha256=hashlib.sha256()
+    sha256.update(string0.encode('utf-8'))
+    res=sha256.hexdigest()
+    return res
+#此处使用sage生成2^16素数
+q=50123
+a=1097
+b=4093
+x_g=40982
+y_g=66
+
+#d=random.randint(1,50121)
+#print(d)
+d1=46007
+d2=39251
+#x_p=(d*x_g)%q
+#y_p=(d*y_g)%q
+#print(x_p)
+#print(y_p)
+x_p=32106
+y_p=29082
+M1='helloworld'
+e1=get_hash(M1)
+M2='nicetomeetyou'
+e2=get_hash(M2)
+
+k=random.randint(2,50123)
+while(gcd(k,q)!=1):
+    k=random.randint(2,50123)
+
+x=(k*x_g)%q
+r1=(int(e1,16)+x)%q
+c1=ModInverse(1+d1,q)
+s1=(c1*(k-r1*d1))%q
+
+r2=(int(e2,16)+x)%q
+c2=ModInverse(1+d2,q)
+s2=(c2*(k-r2*d2))%q
+
+n1=ModInverse(s2+r2,q)
+n2=ModInverse(s1+r1,q)
+
+d2_leaking=((k-s2)*n1)%q
+d1_leaking=((k-s1)*n2)%q
+print(d1_leaking)
+print(d2_leaking)
